18e875f38425d51961b4abe0251e53c9f0d5ec54
David Mateos <cldav.privmath@gmail.com>
2020-05-09 23:38:46 -0500

    removed unnecessary lines

--- R/mod_data_format.R
+++ R/mod_data_format.R
@@ -17,18 +17,16 @@ mod_data_format_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
-    # sidebarPanel
     col_4(
       h4("Here you can choose the format of each column"),
       h6("You can skip this step by clicking the button and we'll guess column type."),
       h6("You can always come back if something went tricky ;)"),
       actionButton(ns("apply"), "Apply formats")
       ),
-    # mainPanel
     col_6(
       rhandsontable::rHandsontableOutput(ns("format"))
     )
-    )#fluidRow
+    )
   )
 }
     
